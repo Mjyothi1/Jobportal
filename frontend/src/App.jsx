68885a9b79c758703e46984e11b553e25226e7fb
nani-tyson <nanityson10@gmail.com>
2025-08-05 01:41:22 +0530

    Navbar Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile'; // placeholder for now
-import Home from './pages/Home'; // optional
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile"; // placeholder for now
+import Home from "./pages/Home"; // optional
 import Jobs from "./pages/Jobs";
-import PostJob from './pages/PostJob';
+import PostJob from "./pages/PostJob";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/post-job" element={<PostJob />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Navbar />  
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/post-job" element={<PostJob />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
